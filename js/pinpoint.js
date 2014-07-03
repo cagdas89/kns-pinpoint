2518a6dc630954a38c649518c5298d5c534b4e04
bugra <bugra.taskaya@ozu.edu.tr>
2014-07-03 18:28:21 +0300

    *

--- js/pinpoint.js
+++ js/pinpoint.js
@@ -2,7 +2,16 @@
 var canvas;
 var ctx;
 var nodes = new Array();
-var clickCounter=0;
+var xCo=new Array();
+var yCo=new Array();
+var clickCounter = 0;
+var xTriggered = 0;
+var checkKeyPressed = false;
+var nameOfContext;
+var removeLastLine=false;
+var area;
+var background;
+
 
 $(function () {
 
@@ -10,97 +19,131 @@ $(function () {
     canvas = document.getElementById("floor-plan-canvas");
     ctx = canvas.getContext("2d");
 
-  /*  canvas = document.getElementById("floor-plan-canvas");
-    var context = canvas.getContext('2d');
-    context.beginPath();
-    context.moveTo(150, 150);
-    context.lineTo(150, 200);
-    context.lineWidth = 10;
-    context.strokeStyle = '#ff0000';
-    context.stroke();
-*/
     // HTML dökümanındaki canvas elementine bir tane kat planı resmini
     // background olarak atıyoruz.
-    var background = new Image();
+    background = new Image();
     background.src = "http://sypiazza.s3.amazonaws.com/images/floors/16_original.png?1391002511";
 
-    background.onload = function() {
+    background.onload = function () {
         // Resim browser tarafından indirilince fire edildiği
         // için burda background 'ı set edip, pinpoint fonksiyonunu
         // yani asıl işi yapacak fonksiyonu çağırabiliriz.
-        ctx.drawImage(background,0,0);
+        ctx.drawImage(background, 0, 0);
         pinpoint();
     };
-
-
 });
 
 
+/*
+function polygonArea(X, Y)
+{
+   area = 0;
+   var j = nodes.length-1;
+
+    for (var i=0; i<numPoints; i++){
+     area = area +  (X[j]+X[i]) * (Y[j]-Y[i]);
+        j = i;  //j is previous vertex to i
+    }
+    return area/2;
+}
+*/
+function redrawStoredLines() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+        ctx.drawImage(background, 0, 0);
+
+
+      if (nodes.length == 0) {
+        return;
+    }
+
+    // redraw each stored line
+    for (var i = 0; i < nodes.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(xCo[i], yCo[i]);
+        ctx.lineTo(xCo[i+1], yCo[i+1]);
+        ctx.stroke();
+    }
+}
 
 function pinpoint() {
 
-         $("#floor-plan-canvas").click(function (e) {
+
+    $("#floor-plan-canvas").click(function (e) {
+        if (checkKeyPressed == false) {
+              var element = canvas;
+            var offsetX = 0, offsetY = 0
+
+            if (element.offsetParent) {
+                do {
+                    offsetX += element.offsetLeft;
+                    offsetY += element.offsetTop;
+                } while ((element = element.offsetParent));
+            }
+
+            var x = e.pageX- offsetX;
+            var y = e.pageY - offsetY;
+
+            if(((x-nodes[0]<50 && x-nodes[0]>0)||(x-nodes[0]<0 && x-nodes[0]>-50)) && ((y-nodes[1]<50 && y-nodes[1]>0)||(y-nodes[1]<0 && y-nodes[1]>-50)) ){
+
+                x=nodes[0];
+                y=nodes[1];
+
+            }
 
 
-                 var element = canvas;
-                 var offsetX = 0, offsetY = 0
+            nodes.push(x);
+            nodes.push(y);
+            xCo.push(x);
+            yCo.push(y);
 
-                 if (element.offsetParent) {
-                     do {
-                         offsetX += element.offsetLeft;
-                         offsetY += element.offsetTop;
-                     } while ((element = element.offsetParent));
-                 }
+            ctx.beginPath();
+            ctx.moveTo(nodes[nodes.length - 4], nodes[nodes.length - 3]);
+            ctx.lineTo(nodes[nodes.length - 2], nodes[nodes.length - 1]);
 
-                var x = e.pageX - offsetX;
-                var y = e.pageY - offsetY;
-
-                nodes.push(x);
-                nodes.push(y);
-
-                ctx.beginPath();
-                ctx.moveTo(nodes[0],nodes[1]);
-             ctx.lineTo(nodes[2], nodes[3]);
-             ctx.lineTo(nodes[4], nodes[5]);
-             ctx.lineTo(nodes[6], nodes[7]);
-             ctx.lineTo(nodes[8], nodes[9]);
-                //ctx.lineTo(nodes[nodes.length-2], nodes[nodes.length-1]);
-                ctx.lineWidth = 2;
-                ctx.strokeStyle = '#ff0000';
-                ctx.stroke();
+           /* ctx.moveTo(xCo[counter], yCo[counter]);
+            ctx.lineTo(xCo[counter+1], yCo[counter+1]);*/
 
 
-                  clickCounter++;
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = '#ff0000';
+            ctx.stroke();
 
-                        /*
-                if (nodes.length == 11) {
-                    nodes.length = 0
-                    clickCounter = 0;
-                }
+            clickCounter++;
 
-                */
-            });
+        }
+    });
 
 
-       if ((clickCounter == 5) && (nodes[0] == nodes[8]) && (nodes[1] == nodes[9])) {
+    $("body").keydown(function (event) {
 
-           var x0 = nodes[0];
-           var y0 = nodes[1];
-           var x1 = nodes[2];
-           var y1 = nodes[3];
-           var x2 = nodes[4];
-           var y2 = nodes[5];
-           var x3 = nodes[6];
-           var y3 = nodes[7];
+        xTriggered++;
+        if (event.which == 65) {   //65: key "A"
 
-           //quadrilateral area formula:
-           var area = ((x0 * y1 - x1 * y0) + (x1 * y2 - x2 * y1) + (x2 * y3 - x3 * y2) + (x3 * y0 - x0 * y3)) / 2;
-            console.log(area);
-
-       }
-    else{console.log("aaaa");}
+            event.preventDefault();
+            checkKeyPressed = true;
+            nameOfContext=prompt("Enter the name: ");
+            console.log(nodes.join());
+            console.log(nameOfContext);
+            checkKeyPressed = false;
+            nodes.length=0;
+        }
 
 
+       else if (event.which == 83) {   //83: key "S"
+
+            event.preventDefault();
+            nodes.pop();
+            nodes.pop();
+            xCo.pop();
+            yCo.pop();
+
+            redrawStoredLines();
+            removeLastLine = true;
+        }
+
+    });
 };
 
 
